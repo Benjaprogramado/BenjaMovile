786617084fc10937e44164a509721ac0418cc688
Benjamin <benjaschell@gmail.com>
2023-09-15 23:26:39 -0300

    loginTemp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent},
   {
-    // path: '',
-    // loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-    path: 'login',
-    loadChildren: () => import('./login/login/login.module').then(m => m.LoginModule)
-  }
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
 ];
 @NgModule({
   imports: [
